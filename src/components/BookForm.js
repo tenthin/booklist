ad2a4d7a8cb30e52d15bf1c854c5a0eb078f216c
Tenzin <tenthinten@gmail.com>
2022-01-09 11:59:01 +0530

    able to input data

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -5,8 +5,15 @@ const NewBookForm = () => {
     const {addBook} = useContext(BookContext)
     const [title,setTitle] = useState('')
     const [author,setAuthor] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addBook(title,author);
+        setTitle('')
+        setAuthor('')
+
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder='book title' value={title}
             onChange={(e)=>setTitle(e.target.value)} required/>
             <input type="text" placeholder='author' value={author}
